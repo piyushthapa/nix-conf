ecd2a7e4907e674a9289b63cb9d9bf00844253e0
Piyush thapa <piyush.codelabs@gmail.com>
2023-10-29 15:12:07 +0545

    change kitty theme

--- home/configs/kitty.nix
+++ home/configs/kitty.nix
@@ -1,37 +1,51 @@
-{config, pkgs, ...}:
-
 {
   enable = true;
+  theme = "Catppuccin-Macchiato";
   settings = {
-    font_size = "12.0";
+    font_size = "15.0";
     font_family = "JetBrains Mono";
     bold_font = "JetBrains Mono";
     italic_font = "JetBrains Mono";
     bold_italic_font = "JetBrains Mono";
-    background = "#1d2021";
-    selection_background="#a89984";
-    foreground = "#83a598";
-    selection_foreground="#282828";
-    cursor="#8ec07c";
-
-
-    color0  = "#002731";
-    color1  = "#d01b24";
-    color2  = "#728905";
-    color3  = "#a57705";
-    color4  = "#2075c7";
-    color5  = "#c61b6e";
-    color6  = "#259185";
-    color7  = "#e9e2cb";
-    color8  = "#001e26";
-    color9  = "#bd3612";
-    color10 = "#465a61";
-    color11 = "#52676f";
-    color12 = "#708183";
-    color13 = "#5856b9";
-    color14 = "#81908f";
-    color15 = "#fcf4dc";
-
+    foreground = "#CAD3F5";
+    background = "#24273A";
+    selection_foreground = "#24273A";
+    selection_background = "#F4DBD6";
+    cursor = "#F4DBD6";
+    cursor_text_color = "#24273A";
+    url_color = "#F4DBD6";
+    active_border_color = "#B7BDF8";
+    inactive_border_color = "#6E738D";
+    bell_border_color = "#EED49F";
+    wayland_titlebar_color = "system";
+    macos_titlebar_color = "system";
+    active_tab_foreground = "#181926";
+    active_tab_background = "#C6A0F6";
+    inactive_tab_foreground = "#CAD3F5";
+    inactive_tab_background = "#1E2030";
+    tab_bar_background = "#181926";
+    mark1_foreground = "#24273A";
+    mark1_background = "#B7BDF8";
+    mark2_foreground = "#24273A";
+    mark2_background = "#C6A0F6";
+    mark3_foreground = "#24273A";
+    mark3_background = "#7DC4E4";
+    color0 = "#494D64";
+    color8 = "#5B6078";
+    color1 = "#ED8796";
+    color9 = "#ED8796";
+    color2 = "#A6DA95";
+    color10 = "#A6DA95";
+    color3 = "#EED49F";
+    color11 = "#EED49F";
+    color4 = "#8AADF4";
+    color12 = "#8AADF4";
+    color5 = "#F5BDE6";
+    color13 = "#F5BDE6";
+    color6 = "#8BD5CA";
+    color14 = "#8BD5CA";
+    color7 = "#B8C0E0";
+    color15 = "#A5ADCB";
   };
 
   keybindings = {
